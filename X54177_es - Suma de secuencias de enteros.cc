36e993c14373657dd4de6d9dd1c5730d920d4ff9
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2021-02-18 11:11:30 +0100

    Create X54177_es - Suma de secuencias de enteros.cc

---
+++ X54177_es - Suma de secuencias de enteros.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+    for (int i = 1; i <= n; ++i) {
+        int sum = 0, y;
+        cin >> y;
+        while (y != x) {
+            sum += y;
+            cin >> y;
+        }
+        string s;
+        getline(cin, s);
+        cout << "La suma de la secuencia " << i << " es " << sum << endl;
+    }
+}
