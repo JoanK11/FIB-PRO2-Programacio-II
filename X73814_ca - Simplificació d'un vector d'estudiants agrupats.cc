f4afecd371188e545cf1fbe7afdca8ef72d121af
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2021-02-25 11:51:31 +0100

    Done

--- X73814_ca - Simplificació d'un vector d'estudiants agrupats.cc
+++ X73814_ca - Simplificació d'un vector d'estudiants agrupats.cc
@@ -1,19 +1,38 @@
+#include <iostream>
 #include <vector>
-#include "Estudiant.hh"
 using namespace std;
 
-void simplificar_vector(vector<Estudiant>& v, int& pos) {
-/* Pre: v = V, v.size() > 0 i les notes d'un mateix estudiant son consecutives */
-/* Post: pos + 1 es el nombre d'estudiants diferents de V; cada estudiant de V
-   nomes hi apareix un cop a v[0..pos], amb la nota mes alta i en el mateix
-   ordre que a V */
-   int n = v.size(), j = 0;
-   for (int i = 1; i < n; ++i) {
-       int dni1 = v[i].consultar_DNI(), dni2 = v[j].consultar_DNI();
-       double nota1 = v[i].consultar_nota(), nota2 = v[j].consultar_nota();
-       if (dni1 == dni2 and nota1 > nota2) v[j].modificar_nota(nota1);
-       else if (dni1 != dni2) ++j, v[j] = v[i];
-   }
-   while (v.size() > j + 1) v.pop_back();
-   pos = v.size();
+vector< pair<int, double> > simplifica(const vector< pair<int, double> >& v) {
+    int n = v.size(), j = 0; // j = posicio de l'ultim element al vector r
+    // Inicialitzem un vector r amb el primer valor de v
+    vector< pair<int, double> > r(1, v[0]);
+    for (int i = 1; i < n; ++i) {
+        r.push_back(v[i]);
+        if (r[j].first == v[i].first) { // Si ja estava el DNI fem pop_back()
+            r.pop_back();
+            // Si la nota es mes alta la canviem
+            if (r[j].second < v[i].second) r[j].second = v[i].second;
+        }
+        else ++j; // S'ha afegit una nova posiciÃ³ al vector
+    }
+    return r;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    // Llegim el vector original
+    vector< pair<int, double> > v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i].first >> v[i].second;
+        if (v[i].second > 10) v[i].second = -1; // Nota = {-1}, [0, 10]
+    }
+    
+    vector< pair<int, double> > result = simplifica(v);
+    n = result.size();
+    for (int i = 0; i < n; ++i) {
+        cout << result[i].first << ' ';
+        if (result[i].second < 0) cout << "NP" << endl;
+        else cout << result[i].second << endl;
+    }
 }
