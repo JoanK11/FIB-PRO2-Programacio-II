ee30773d34f696066810005c193fd80efc785c0d
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2021-03-11 14:14:30 +0100

    Done

---
+++ X13425 - Distribucio justa de cues/program.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+#include "CuaIOParInt.hh"
+#include "ParInt.hh"
+using namespace std;
+
+void repartir(queue<ParInt>& q, queue<ParInt>& q1, queue<ParInt>& q2) {
+    int t1 = 0, t2 = 0;
+    while (not q.empty()) {
+        if (t2 < t1) {
+            q2.push(q.front());
+            t2 += q.front().segon();
+        }
+        
+        else {
+            q1.push(q.front());
+            t1 += q.front().segon();
+        }
+        
+        q.pop();
+    }
+}
+
+int main() {
+    queue<ParInt> q;
+    llegirCuaParInt(q);
+    
+    queue<ParInt> q1, q2;
+    repartir(q, q1, q2);
+
+    escriureCuaParInt(q1);
+    cout << endl;
+    escriureCuaParInt(q2);
+}
