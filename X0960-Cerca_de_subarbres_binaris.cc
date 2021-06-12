3f11b21b7b1e39cce11bd947932cdec5fd2ea3f8
Joan Caballero <70658820+JoanK11@users.noreply.github.com>
2021-06-12 12:13:09 +0200

    Create X0960-Cerca_de_subarbres_binaris.cc
    Problema del Jutge X0960_ca - Cerca de subarbres binaris
    https://jutge.org/problems/X09609_ca

---
+++ X0960-Cerca_de_subarbres_binaris.cc
@@ -0,0 +1,16 @@
+static void sub_arrel_i(node_arbre* n, node_arbre* &r, const T& x, int dist, int& min_dist) {
+    if (n == nullptr) return;
+    if (n->info == x and (min_dist == -1 or dist < min_dist)) {
+        r = copia_node_arbre(n);
+        min_dist = dist;
+    }
+    else {
+        sub_arrel_i(n->segE, r, x, dist + 1, min_dist);
+        sub_arrel_i(n->segD, r, x, dist + 1, min_dist);
+    }
+}
+
+void sub_arrel(Arbre& asub, const T& x) {
+    int min_dist = -1;
+    sub_arrel_i(primer_node, asub.primer_node, x, 0, min_dist);
+}
